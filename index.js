b0054ba1938a7bc4999502245e951edb93fecc3b
Daniel Alejandro More Cespedes <dmorelwebdesign@gmail.com>
2020-02-11 00:34:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,44 @@
+function add( a, b){
+   var number = b + a
+  console.log(number)
+ }
+function subtract( a, b){
+   var number = a - b
+console.log(number)
+ }
+function multiply(a, b){
+  var number = b * a
+  console.log(number)
+}
+function divide(a, b){
+  var number = a / b
+  console.log(number)
+}
+
+function increment(a){
+  var number = a += 4
+  console.log(number)
+}
+
+function decrement(a){
+  var number = a -= 4
+  console.log(number)
+}
+function makeInt(n){
+ console.log(parseInt(n, 10))
+
+}
+
+function preserveDecimal(n){
+ console.log(parseFloat(n))
+  }
+
+ add(34, 87)
+ subtract(356, 159)
+ multiply ( 4, 56)
+ divide (10, 5)
+increment(34)
+decrement(166)
+makeInt( "857")
+preserveDecimal('80.123999')
+preserveDecimal("I'm alive")
