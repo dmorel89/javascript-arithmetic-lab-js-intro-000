d17ab8b336e6f23618a98150b37b59b993b17760
Daniel Alejandro More Cespedes <dmorelwebdesign@gmail.com>
2020-02-11 00:09:18 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function add( a, b){
+   var number = b + a
+  console.log(number)
+ }
+function subtract( a, b){
+   var number = a - b
+console.log(number)
+ }
+function multiply(a, b){
+  var number = b * a
+  console.log(number)
+}
+function divide(a, b){
+  var number = a / b
+  console.log(number)
+}
+
+function increment(a){
+  var number = a += 4
+  console.log(number)
+}
+
+function decrement(a){
+  var number = a -= 4
+  console.log(number)
+}
+
+function makeInt(n){
+console.log(parseInt(n))
+
+}
